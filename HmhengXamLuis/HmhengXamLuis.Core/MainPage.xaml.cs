495c994affdaa6b88400c0db86555360ca5e4e3b
Marvin Heng <Marvin.Heng@avire-global.com>
2017-11-06 19:00:47 +0800

    Add project files.

---
+++ HmhengXamLuis/HmhengXamLuis.Core/MainPage.xaml.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using Xamarin.Forms;
+using Newtonsoft.Json;
+using HmhengXamLuis.Core.Models;
+
+namespace HmhengXamLuis
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            btnConnect.Clicked += MakeRequest;
+        }
+
+        public async void MakeRequest(object sender, EventArgs e)
+        {
+            var client = new HttpClient();
+            var queryString = HttpUtility.ParseQueryString(string.Empty);
+
+            // This app ID is for a public sample app that recognizes requests to turn on and turn off lights
+            var luisAppId = "<replace with your LUIS App ID here>";
+            var subscriptionKey = "<replace with your LUIS subscription Key here>";
+
+            // The request header contains your subscription key
+            client.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", subscriptionKey);
+
+            // The "q" parameter contains the utterance to send to LUIS
+            queryString["q"] = txtMessage.Text;
+
+            // These optional request parameters are set to their default values
+            queryString["timezoneOffset"] = "0";
+            queryString["verbose"] = "false";
+            queryString["spellCheck"] = "false";
+            queryString["staging"] = "false";
+
+            var uri = "https://southeastasia.api.cognitive.microsoft.com/luis/v2.0/apps/" + luisAppId + "?" + queryString; //remember to change to the region that you set in LUIS.ai
+            var response = await client.GetAsync(uri);
+
+            var strResponseContent = await response.Content.ReadAsStringAsync();
+
+            //// Display the JSON result from LUIS
+            //Console.WriteLine(strResponseContent.ToString());
+
+            try
+            {
+                lblIntent.Text = "";
+                lblEntities.Text = "";
+                LuisResponse luisResponse = JsonConvert.DeserializeObject<LuisResponse>(strResponseContent);
+                if (luisResponse != null)
+                {
+                    if (luisResponse.topScoringIntent != null)
+                    {
+                        lblIntent.Text = luisResponse.topScoringIntent.intent;
+                    }
+
+                    if(luisResponse.entities.Count() > 0)
+                    {
+                        foreach (var entities in luisResponse.entities)
+                        {
+                            lblEntities.Text += entities.entity + "(" + entities.type + ")\n";
+                        }
+                    }
+                }
+            }catch(Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+
+    }
+}
---
+++ HmhengXamLuis/HmhengXamLuis.Core/Models/LuisResponse.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HmhengXamLuis.Core.Models
+{
+    public class LuisResponse
+    {
+        public string query { get; set; }
+        public TopScoringIntent topScoringIntent { get; set; }
+        public List<Intent> intents { get; set; }
+        public List<Entity> entities { get; set; }
+    }
+
+    public class TopScoringIntent
+    {
+        public string intent { get; set; }
+        public double score { get; set; }
+    }
+
+    public class Intent
+    {
+        public string intent { get; set; }
+        public double score { get; set; }
+    }
+
+    public class Value
+    {
+        public string timex { get; set; }
+        public string type { get; set; }
+        public string value { get; set; }
+    }
+
+    public class Resolution
+    {
+        public List<Value> values { get; set; }
+    }
+
+    public class Entity
+    {
+        public string entity { get; set; }
+        public string type { get; set; }
+        public int startIndex { get; set; }
+        public int endIndex { get; set; }
+        public double score { get; set; }
+        public Resolution resolution { get; set; }
+    }
+
+}
